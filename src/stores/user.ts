558eee347e25339f3331e5fedb9153a5bb663fcb
Ubuntu <ubuntu@ip-172-31-25-120.ec2.internal>
2023-09-20 05:42:03 +0000

    fc

---
+++ src/stores/user.ts
@@ -0,0 +1,36 @@
+import { defineStore, acceptHMRUpdate } from "pinia";
+
+export const useStore = defineStore("state", () => {
+  const state = reactive({
+    name: "",
+    notifications: [] as { message: string; status: string }[],
+    eventSource: null as EventSource | null,
+    messages: [] as string[],
+  });
+
+  const setState = (newState: any) => {
+    Object.assign(state, newState);
+  };
+
+  const notify = (message: string, status: string) => {
+    state.notifications.push({ message, status });
+  };
+
+  watchEffect(() => {
+    if (state.eventSource) {
+      state.eventSource.onmessage = (event: MessageEvent) => {
+        state.messages.push(event.data);
+      };
+    }
+  });
+
+  return {
+    state,
+    notify,
+    setState,
+  };
+});
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useStore, import.meta.hot));
+}
